944aa3d4d5c3df56ce4a8f661360e67fd0503ce9
rashmimahat <rashmimahat.81@gmail.com>
2024-05-05 07:01:04 +0545

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class FirstJavaProgram {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("WelCome to the JavaWorld");
+		System.out.println(10 + 20);
+	}
+
+}
